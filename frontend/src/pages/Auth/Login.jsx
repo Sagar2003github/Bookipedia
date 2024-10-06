da3243736706d6b2299433272dda45a907bbd0c2
Sagar <sagarjms285@gmail.com>
2024-10-06 10:23:40 +0530

    first commit

---
+++ frontend/src/pages/Auth/Login.jsx
@@ -0,0 +1,122 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../../components/Loader";
+import { useLoginMutation } from "../../redux/api/usersApiSlice";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+import { toast } from "react-toastify";
+import Logo from "../../../../uploads/BOOKlogo.png"; // Import your logo
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await login({ email, password }).unwrap();
+      console.log(res);
+      dispatch(setCredentials({ ...res }));
+      navigate(redirect);
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  return (
+    <div>
+      <section className="flex flex-wrap items-center justify-between">
+        {/* Left side form */}
+        <div className="w-full lg:w-1/2 p-10 ml-auto lg:ml-16">
+          <h1 className="text-2xl font-semibold mb-4">Sign In</h1>
+
+          <form onSubmit={submitHandler} className="w-full" autoComplete="off">
+            <div className="my-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-white"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="mt-1 p-2 border rounded w-full"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="mt-1 p-2 border rounded w-full"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button
+              disabled={isLoading}
+              type="submit"
+              className="bg-pink-500 text-white px-4 py-2 rounded cursor-pointer my-4"
+            >
+              {isLoading ? "Signing In..." : "Sign In"}
+            </button>
+
+            {isLoading && <Loader />}
+          </form>
+
+          <div className="mt-4">
+            <p className="text-white">
+              New Customer?{" "}
+              <Link
+                to={redirect ? `/register?redirect=${redirect}` : "/register"}
+                className="text-pink-500 hover:underline"
+              >
+                Register
+              </Link>
+            </p>
+          </div>
+        </div>
+
+        {/* Right side image */}
+        <div className="hidden lg:block w-100% lg:w-1/3 pr-14">
+          <img
+            src={Logo}  // Use your logo here
+            alt="Logo"
+            className="h-screen object-cover w-full rounded-lg"
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/pages/Auth/Register.jsx
@@ -0,0 +1,162 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../../components/Loader";
+import { useRegisterMutation } from "../../redux/api/usersApiSlice";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+import { toast } from "react-toastify";
+import Logo from "../../../../uploads/BOOKlogo.png";
+
+const Register = () => {
+  const [username, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [register, { isLoading }] = useRegisterMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+    } else {
+      try {
+        const res = await register({ username, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate(redirect);
+        toast.success("User successfully registered");
+      } catch (err) {
+        console.log(err);
+        toast.error(err.data.message);
+      }
+    }
+  };
+
+  return (
+    <section className="flex flex-wrap items-center justify-between">
+      {/* Left side form */}
+      <div className="w-full lg:w-1/2 p-10 pr-16 ml-auto lg:ml-16"> {/* pr-16 added for right padding */}
+        <h1 className="text-2xl font-semibold mb-4">Register</h1>
+
+        <form onSubmit={submitHandler} className="w-full" autoComplete="off">
+          <div className="my-4">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter name"
+              value={username}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className="my-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-white"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="my-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="my-4">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-white"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="bg-pink-500 text-white px-4 py-2 rounded cursor-pointer my-4"
+          >
+            {isLoading ? "Registering..." : "Register"}
+          </button>
+
+          {isLoading && <Loader />}
+        </form>
+
+        <div className="mt-4">
+          <p className="text-white">
+            Already have an account?{" "}
+            <Link
+              to={redirect ? `/login?redirect=${redirect}` : "/login"}
+              className="text-pink-500 hover:underline"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+
+      {/* Right side image */}
+      <div className="hidden lg:block w-full lg:w-1/3 pr-16" >
+      <img
+            src={Logo}  // Use your logo here
+            alt="Logo"
+            className="h-screen object-cover w-full rounded-lg"
+          />
+      </div>
+    </section>
+  );
+};
+
+export default Register;
